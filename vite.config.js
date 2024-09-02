e5b4f86d62c2294fda142016356e5c8596e87baa
rjsah09 <rjsah09@naver.com>
2024-09-02 15:54:32 +0900

    Feat: viteConfig proxy 추가

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: '../parkingplace/src/main/resources/static'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080'
+      }
     }
   }
 })
