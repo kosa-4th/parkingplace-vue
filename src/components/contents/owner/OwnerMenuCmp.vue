0556ea0df8db9b24e9f86f86bd9f848aa4dbe049
rjsah09 <rjsah09@naver.com>
2024-09-04 16:16:37 +0900

    Feat: 업주 사이드바

--- src/components/contents/owner/OwnerMenuCmp.vue
+++ src/components/contents/owner/OwnerMenuCmp.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <h3>OwnerMenuCmp</h3>
+  <div id="navigation-wrapper">
+    <div id="owner-info"></div>
+    <div id="link-list">
+      <ul>
+        <li></li>
+        <li>수익 통계</li>
+        <li>주차장 관리</li>
+        <li>예약 관리</li>
+        <li>문의 관리</li>
+      </ul>
+    </div>
   </div>
 </template>
 
