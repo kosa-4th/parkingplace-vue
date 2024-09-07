48d3227452adc512b8435c9af2c3144db6c37279
gaengmin <momo5736@naver.com>
2024-09-07 22:31:16 +0900

    Feat&Design: Reservation
    Feat&Design: Reservation

--- src/components/contents/user/ReservationCmp.vue
+++ src/components/contents/user/ReservationCmp.vue
@@ -3,171 +3,449 @@
     시작 일자: 2024.09.03
     설명 : 주차장 예약 컴포넌트
     ---------------------
-    2024.09.03 김경민 | 피그마를 확인 후 DatePicker / TimePicker를 활용한 예약 시스템 앞단 만만들기
+    2024.09.03 김경민 | 피그마를 확인 후 DatePicker
+    2022.09.07 김경민 | 시간선택 및 전체적인 디자인
 -->
 <template>
-  <div>
+  <div class = "container">
     <!-- 전역으로 등록된 DatePicker 사용 -->
-    <!-- 전역으로 등록된 TimePicker 사용 -->
+    <h3>{{ parkingLotName }}</h3>
     <!-- 각각 기능 설명-->
     <!-- 선택된 날짜를 selectedDate에 바인딩 -->
     <!-- 오늘 이후의 날짜만 선택 가능 -->
     <!-- 30일 후까지 선택 가능 -->
     <!-- 날짜 형식을 yyyy-MM-dd로 설정 -->
     <!-- 시간 선택 기능 비활성화 -->
-    <DatePicker
-      locale="ko"
-      v-model="entranceDate"
-      :min-date="today"
-      :max-date="maxDate"
-      :format="'yyyy-MM-dd'"
-      :enable-time-picker="false"
-    >
-    </DatePicker>
-    <TimePicker v-model="entranceTime" format="HH:mm"
-                :minute-interval="30" />
+    <div class="row">
+      <div class = "col-md-12">
+        <!-- 차량 번호 선택 -->
+        <select v-model="selectedCarNumber" class="form-control">
+          <option value="" disabled>차량 번호 선택</option>
+          <option v-for="option in carOptions" :key="option" :value="option">
+            {{ option }}
+          </option>
+        </select>
+      </div>
+    </div>
+    <div class="row">
+      <div class = "col-md-12">
+        <DatePicker
+          locale="ko"
+          v-model="selectedDate"
+          :min-date="today"
+          :max-date="maxDate"
+          :format="'yyyy-MM-dd'"
+          :enable-time-picker="false">
+        </DatePicker>
+      </div>
+    </div>
+    <!-- 입차시간 -->
+    <div class="time-picker-container mt-2">
+      <div class="row g-0">
+        <h6>입차시간</h6>
+        <div class="col-12 col-md-12 text-center">
+          <div class="time-picker-grid">
+            <button
+              v-for="hour in entranceAvailableHours"
+              :key="hour"
+              @click="selectEntranceHour(hour)"
+              :class="['time-picker-btn', { 'selected': hour === selectedEntranceHour }]"
+            >
+              {{ hour }}
+            </button>
+          </div>
+        </div>
+      </div>
+      <hr>
+      <div class="row g-0">
+        <div class="col-12 col-md-12">
+          <div class="time-picker-grid text-center">
+            <button
+              v-for="minute in entranceAvailableMinutes"
+              :key="minute"
+              @click="selectEntranceMinute(minute)"
+              :class="['time-picker-btn', { 'selected': minute === selectedEntranceMinute }]"
+            >
+              {{ minute < 10 ? '0' + minute : minute }}
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- Confirm Button -->
+      <div class="col-12 text-center mt-1">
+        <button @click="confirmEntranceTime" class="btn confirm-btn">입력</button>
+      </div>
+    </div>
 
-    <p>입차 날짜 및 시간: {{ entranceDateTime }} </p>
+    <!-- 출차시간 -->
+    <div class="time-picker-container mt-2">
+      <div class="row g-0">
+        <h6>출차시간</h6>
+        <div class="col-12 col-md-12 text-center">
+          <div class="time-picker-grid">
+            <button
+              v-for="hour in exitAvailableHours"
+              :key="hour"
+              @click="selectExitHour(hour)"
+              :class="['time-picker-btn', { 'selected': hour === selectedExitHour }]"
+            >
+              {{ hour }}
+            </button>
+          </div>
+        </div>
+      </div>
+      <hr>
+      <div class="row g-0">
+        <div class="col-12 col-md-12">
+          <div class="time-picker-grid text-center">
+            <!-- 출차 시간을 선택하는 버튼 -->
+            <button
+              v-for="minute in exitAvailableMinutes"
+              :key="minute"
+              @click="selectExitMinute(minute)"
+              :class="['time-picker-btn', { 'selected': minute === selectedExitMinute }]"
+            >
+              {{ minute < 10 ? '0' + minute : minute }}
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- Confirm Button -->
+      <div class="col-12 text-center mt-1">
+        <button @click="confirmExitTime" class="btn confirm-btn">입력</button>
+      </div>
+    </div>
 
-    <DatePicker
-      locale="ko"
-      v-model="exitDate"
-      :min-date="today"
-      :max-date="maxDate"
-      :format="'yyyy-MM-dd'"
-      :enable-time-picker="false"
-    ></DatePicker>
-    <TimePicker v-model="exitTime" format="HH:mm"
-                :minute-interval="30" />
-    <p>출차 날짜 및 시간:{{ exitDateTime }} </p>
+    <div class="row">
+      <div class = "col-md-3">
+        <label>입차일자</label>
+        <Strong>{{ formattedSelectedDate }}</Strong>
+      </div>
+      <div class = "col-md-3">
+        <label>총 주차시간</label>
+        <Strong>{{ totalTime }}</Strong>
+      </div>
+      <div class = "col-md-3">
+        <label>입차시간</label>
+        <Strong>{{ selectedEntranceTime }}</Strong>
+      </div>
+      <div class = "col-md-3">
+        <label>출차시간</label>
+        <Strong>{{ selectedExitTime }}</Strong>
+      </div>
 
-    <p>총 예약 시간 : {{ totalReservationTime }}</p>
+    </div>
 
-    <label>차량번호</label>
-    <select v-model="selectedOption">
-      <option v-for="option in options" :key="option" :value="option">
-        {{ option }}
-      </option>
-    </select>
+    <div class="row">
 
-    <label>부가서비스</label>
-    <label v-for="option in options" :key="option.value">
-      <input type="checkbox" :value="option.value" v-model="selectedOption" />
-      {{ option.label }}
-    </label>
+      <!-- 부가서비스 선택 -->
+      <div class="col-md-6">
+        <label>부가서비스</label>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="washService"
+            v-model="services.wash"
+          />
+          <label class="form-check-label" for="washService">세차</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="maintenanceService"
+            v-model="services.maintenance"
+          />
+          <label class="form-check-label" for="maintenanceService">기본차량정비</label>
+        </div>
+      </div>
+    </div>
+
 
     <br>
     <label>가격</label>
-
-
-    <p>Selected Option: {{ selectedOption }}</p>
 
     <button class="btn btn-primary" :disabled="totalReservationTime <= 0" @click="submitReservation">
       예약하기
     </button>
   </div>
 </template>
-
 <script>
-import { computed, ref, watch } from 'vue'
-
 export default {
-  setup() {
-    // 현재 날짜 가져오기
-    const today = new Date()
-
-    // select 박스에 표시될 옵션들
-    const options = ref([1, 2, 3])
-
-    // 선택된 값을 저장할 변수
-    const selectedOption = ref(null)
-
-    // 오늘 날짜로부터 30일 후의 날짜 계산
-    const maxDate = new Date()
-    maxDate.setDate(today.getDate() + 30)
-
-    // 가장 가까운 30분과 1시간으로 시간 설정하는 함수
-    function roundToNearestMinutes(date, minutes) {
-      const ms = 1000 * 60 * minutes
-      return new Date(Math.ceil(date.getTime() / ms) * ms)
-    }
-
-    const now = new Date()
-
-    // entranceTime: 현재 시간에 가장 가까운 30분으로 설정
-    const entranceTimeDate = roundToNearestMinutes(now, 30)
-    const entranceTime = ref(entranceTimeDate.toTimeString().substring(0, 5)) // "HH:mm" 형식으로 포맷
-
-    // exitTime: 현재 시간에 가장 가까운 1시간으로 설정
-    const exitTimeDate = roundToNearestMinutes(now, 60)
-    const exitTime = ref(exitTimeDate.toTimeString().substring(0, 5)) // "HH:mm" 형식으로 포맷
-
-    // 날짜와 시간을 ref로 초기화
-    const entranceDate = ref(today)
-    const exitDate = ref(today)
-
-    // formattedeEntranceDate와 entranceTime을 조합하여 하나의 DateTime 문자열 생성
-    const entranceDateTime = computed(() => {
-      const date = entranceDate.value.toISOString().split('T')[0] // yyyy-MM-dd 형식의 날짜
-      return `${date} ${entranceTime.value}` // yyyy-MM-dd HH:mm 형식으로 결합
-    })
-
-    // formattedeExitDate와 exitTime을 조합하여 하나의 DateTime 문자열 생성
-    const exitDateTime = computed(() => {
-      const date = exitDate.value.toISOString().split('T')[0] // yyyy-MM-dd 형식의 날짜
-      return `${date} ${exitTime.value}` // yyyy-MM-dd HH:mm 형식으로 결합
-    })
-
-    const totalReservationTime = ref(0)
-
-    // 두 날짜를 비교하고 exitDateTime이 entranceDateTime보다 이른 경우 경고
-    watch([entranceDateTime, exitDateTime], ([newEntranceDateTime, newExitDateTime]) => {
-      const now = new Date()
-      if (new Date(newEntranceDateTime) < now) {
-        totalReservationTime.value = 0
-        alert('입장 시간이 현재 시점보다 이전입니다.')
-      } else if (new Date(newExitDateTime) < now) {
-        totalReservationTime.value = 0
-        alert('퇴장 시간이 현재 시점보다 이전입니다.')
-      } else if (newExitDateTime <= newEntranceDateTime) {
-        totalReservationTime.value = 0
-        alert('퇴장 시간이 입장 시간보다 이른 시간입니다. 다시 입력해주세요.')
-      } else {
-        const diffInMinutes = (new Date(newExitDateTime) - new Date(newEntranceDateTime)) / 1000 / 60
-        const hours = Math.floor(diffInMinutes / 60)
-        const minutes = diffInMinutes % 60
-        totalReservationTime.value = `${hours}시간 ${minutes}분`
-      }
-    })
-
-    // 반환할 데이터
+  data() {
     return {
-      today,
-      maxDate,
-      entranceDate,
-      exitDate,
-      entranceTime,
-      exitTime,
-      entranceDateTime,
-      exitDateTime,
-      totalReservationTime,
-      options,
-      selectedOption
+      parkingLotName : "테스트주차장",
+
+      selectedCarNumber: '',  // 선택된 차량 번호
+      carOptions: ['123가 4567', '987나 1234', '456다 7890'],  // 차량 번호 옵션들
+      services: {
+        wash: false,  // 세차 여부
+        maintenance: true,  // 기본차량정비 여부
+      },
+
+
+      today: new Date(),
+      maxDate: new Date(new Date().setDate(new Date().getDate() + 30)), // 30일 후까지 선택 가능
+      selectedDate : null,
+
+      entanceDateTime : '',
+      extitDateTime : '',
+      selectedEntranceTime : '',
+      selectedExitTime : '',
+      totalTime : '00:00',
+
+      // 입차 시간 관련 데이터
+      entranceBusinessStartTime: 14, // 영업 시작 시간 (14시)
+      entranceBusinessEndTime: 22, // 영업 종료 시간 (22시)
+      entranceAvailableHours: [], // 입차 가능한 시간 배열
+      entranceAvailableMinutes: [0, 30], // 입차 가능한 분 배열
+      selectedEntranceHour: null,
+      selectedEntranceMinute: null,
+
+      // 출차 시간 관련 데이터
+      exitBusinessStartTime: 14, // 영업 시작 시간 (14시)
+      exitBusinessEndTime: 22, // 영업 종료 시간 (22시)
+      exitAvailableHours: [], // 출차 가능한 시간 배열
+      exitAvailableMinutes: [0, 30], // 출차 가능한 분 배열
+      selectedExitHour: null,
+      selectedExitMinute: null,
+      totalReservationTime: 0,
+      options: [1, 2, 3], // 선택 박스에 표시될 옵션들
+      selectedOption: null,
+    };
+  },
+  created() {
+    this.generateEntranceAvailableHours();
+    this.generateExitAvailableHours();
+  },
+
+  methods: {
+    // 입차 가능한 시간 생성
+    generateEntranceAvailableHours() {
+      this.entranceAvailableHours = Array.from(
+        { length: this.entranceBusinessEndTime - this.entranceBusinessStartTime + 1 },
+        (_, i) => (this.entranceBusinessStartTime + i).toString().padStart(2, '0')
+      );
+    },
+    // 출차 가능한 시간 생성
+    generateExitAvailableHours() {
+      this.exitAvailableHours = Array.from(
+        { length: this.exitBusinessEndTime - this.exitBusinessStartTime + 1 },
+        (_, i) => (this.exitBusinessStartTime + i).toString().padStart(2, '0')
+      );
+    },
+    selectEntranceHour(hour) {
+      this.selectedEntranceHour = hour;
+    },
+    selectEntranceMinute(minute) {
+      this.selectedEntranceMinute = minute;
+    },
+
+    confirmEntranceTime() {
+      // selectedEntranceHour, selectedEntranceMinute, entranceDate가 null이 아닌지 확인
+      if (this.selectedEntranceHour !== null && this.selectedEntranceMinute !== null && this.selectedDate !== null) {
+        // 선택한 날짜와 시간을 합쳐서 Date 객체로 변환
+        const selectedEntranceDate = new Date(this.selectedDate);
+        selectedEntranceDate.setHours(parseInt(this.selectedEntranceHour), parseInt(this.selectedEntranceMinute), 0, 0);
+
+        // 유효한 Date 객체인지 확인
+        if (isNaN(selectedEntranceDate.getTime())) {
+          alert('유효하지 않은 날짜입니다.');
+          return;
+        }
+
+        // 출차 시간과 비교
+        if (this.selectedExitHour !== null && this.selectedExitMinute !== null && this.exitDate !== null) {
+          const selectedExitDate = new Date(this.exitDate);
+          selectedExitDate.setHours(parseInt(this.selectedExitHour), parseInt(this.selectedExitMinute), 0, 0);
+
+          if (selectedEntranceDate >= selectedExitDate) {
+            alert('입차 시간이 출차 시간보다 늦을 수 없습니다.');
+            return;
+          }
+        }
+
+        // 현재 시간과 비교
+        const now = new Date();
+        if (selectedEntranceDate <= now) {
+          alert('입차 시간이 현재 시간보다 빠를 수 없습니다.');
+          return;
+        }
+
+        // 정상적으로 선택된 시간을 저장하고, entranceDateTimeResult 생성
+        const formattedDate = selectedEntranceDate.toISOString().split('T')[0]; // 'yyyy-MM-dd' 형식으로 변환
+        const formattedTime = `${this.selectedEntranceHour.toString().padStart(2, '0')}:${this.selectedEntranceMinute.toString().padStart(2, '0')}`; // 'HH:mm' 형식으로 변환
+        this.entranceDateTimeResult = `${formattedDate} ${formattedTime}`; // 날짜와 시간을 결합하여 'yyyy-MM-dd HH:mm' 형식으로 저장
+        this.selectedEntranceTime = `${formattedTime}`;
+        alert(`입차 시간이 성공적으로 설정되었습니다: ${this.entranceDateTimeResult}`);
+      } else {
+        alert('입차 시간 및 날짜를 모두 선택해 주세요.');
+      }
+    },
+
+    selectExitHour(hour) {
+      this.selectedExitHour = hour;
+    },
+    selectExitMinute(minute) {
+      this.selectedExitMinute = minute;
+    },
+    confirmExitTime() {
+      if (this.selectedExitHour !== null && this.selectedExitMinute !== null && this.selectedEntranceHour !== null && this.selectedEntranceMinute !== null && this.selectedDate !== null) {
+        // 선택한 입차 및 출차 날짜와 시간을 결합
+        const selectedEntranceDate = new Date(this.selectedDate);
+        selectedEntranceDate.setHours(parseInt(this.selectedEntranceHour), parseInt(this.selectedEntranceMinute), 0, 0);
+
+        const selectedExitDate = new Date(this.selectedDate);
+        selectedExitDate.setHours(parseInt(this.selectedExitHour), parseInt(this.selectedExitMinute), 0, 0);
+
+        // 입차 시간이 출차 시간보다 빠르면 경고
+        if (selectedExitDate <= selectedEntranceDate) {
+          alert('출차 시간이 입차 시간보다 빠를 수 없습니다.');
+          this.selectedExitHour = null;
+          this.selectedExitMinute = null;
+          this.selectedExitTime = '';  // 초기화
+        } else {
+          // 정상적으로 선택된 시간을 저장
+          const formattedExitTime = `${this.selectedExitHour.toString().padStart(2, '0')}:${this.selectedExitMinute.toString().padStart(2, '0')}`; // 'HH:mm' 형식으로 변환
+          this.selectedExitTime = formattedExitTime;
+          this.exitDateTimeResult = `${this.selectedDate.toISOString().split('T')[0]} ${formattedExitTime}`; // 'yyyy-MM-dd HH:mm' 형식으로 저장
+          alert(`출차 시간이 성공적으로 설정되었습니다: ${this.exitDateTimeResult}`);
+        }
+      } else {
+        alert('입차 시간 및 출차 시간을 모두 선택해 주세요.');
+      }
+    },
+    calculateTotalTime() {
+      if (this.selectedEntranceTime && this.selectedExitTime) {
+        // 시간 문자열을 분리하여 시간과 분으로 나눔
+        const [entranceHour, entranceMinute] = this.selectedEntranceTime.split(':').map(Number);
+        const [exitHour, exitMinute] = this.selectedExitTime.split(':').map(Number);
+
+        // Date 객체를 사용하여 시간 차이 계산
+        const entranceDateTime = new Date();
+        entranceDateTime.setHours(entranceHour, entranceMinute, 0, 0);
+        const exitDateTime = new Date();
+        exitDateTime.setHours(exitHour, exitMinute, 0, 0);
+
+        // 시간 차이를 밀리초로 계산
+        const diffInMs = exitDateTime - entranceDateTime;
+
+        // 입차 시간이 출차 시간보다 빠를 경우 경고
+        if (diffInMs <= 0) {
+          alert('출차 시간이 입차 시간보다 빠를 수 없습니다.');
+          this.totalTime = '00:00'; // 기본값
+          return;
+        }
+
+        // 시간 차 계산
+        const diffInMinutes = Math.floor(diffInMs / 1000 / 60);
+        const hours = Math.floor(diffInMinutes / 60);
+        const minutes = diffInMinutes % 60;
+
+        // 최종 주차 시간 출력
+        this.totalTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+      } else {
+        this.totalTime = '00:00'; // 기본값
+      }
     }
-  }
-}
+  },
+  watch: {
+    selectedEntranceTime() {
+      this.calculateTotalTime();
+    },
+    selectedExitTime() {
+      this.calculateTotalTime();
+    }
+  },
+  computed: {
+    // 선택된 날짜를 형식에 맞게 변환하는 computed 속성
+    formattedSelectedDate() {
+      return this.selectedDate
+        ? this.selectedDate.toISOString().split('T')[0]
+        : '';
+    },
+  },
+};
 </script>
-
 <style scoped>
-.my-datepicker {
-  border: 2px solid #2ecc71;
-  border-radius: 10px;
-  padding: 5px;
+
+.time-picker-btn {
+  width: 20px;
+  height: 20px;
+  margin-bottom: 6px;
+  margin-left: 6%;
+  margin-right : 6%;
+  border-radius: 50%;
+  border: 1px solid hsl(265, 74%, 65%);
+  background-color: white;
+  color: #9A64E8;
+  font-size: 11px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background-color 0.3s, color 0.3s, box-shadow 0.3s;
 }
 
-.my-timepicker {
-  border: 2px solid #e74c3c;
-  border-radius: 10px;
-  padding: 5px;
+.time-picker-btn.selected {
+  background-color: #9A64E8;
+  color: white;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
+}
+
+.confirm-btn {
+  background-color: #28a745;
+  color: white;
+  padding: 3px 20px;
+  border-radius: 50px;
+  font-size: 12px;
+  border: none;
+  transition: background-color 0.3s;
+}
+
+.confirm-btn:hover {
+  background-color: #218838;
+}
+
+.time-picker-grid {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr); /* 한 줄에 6개씩 */
+  gap: 5px; /* 버튼 사이 간격 */
+  justify-items: center;
+}
+
+.time-picker-container {
+  border: 2px solid #be96ff; /* 테두리 색상 */
+  border-radius: 15px; /* 둥근 모서리 */
+  padding: 10px; /* 내부 여백 */
+  margin-top: 10px; /* 위쪽 여백 */
+  background-color: white; /* 배경색 */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* 그림자 */
+  margin-left : 10px;
+  margin-right : 10px;
+}
+/* DatePicker 버튼을 보라색으로 스타일링 */
+.datepicker-btn {
+  background-color: #9A64E8 !important; /* 보라색 배경 */
+  color: white !important;              /* 글자색을 흰색으로 변경 */
+  border-radius: 4px;                   /* 둥근 모서리 */
+  border: none;                         /* 테두리 제거 */
+  padding: 10px 20px;                   /* 버튼 패딩 조정 */
+}
+
+.datepicker-btn:hover {
+  background-color: #7a52b6 !important; /* hover 시 더 어두운 보라색 */
+}
+
+.datepicker-btn:focus {
+  outline: none;
+  box-shadow: 0 0 0 2px rgba(154, 100, 232, 0.5); /* focus 시 보라색 그림자 */
+}
+.form-check-input {
+  margin-right: 5px;
+}
+.form-check-label {
+  margin-right: 20px;
 }
 </style>
