54cd4ef8f0498df8af05fe57dfd3ddf4e41fa596
gaengmin <momo5736@naver.com>
2024-10-03 18:32:38 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,57 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+export default defineConfig(({ mode }) => {
+  // mode에 따라 환경 변수를 로드 (development, production 등)
+  const env = loadEnv(mode, process.cwd());
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    // outDir: '../parkingplace-spring/src/main/resources/static'
-    outDir: 'dist', // Vite의 빌드 결과물이 'dist' 폴더에 생성됩니다.
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: `${import.meta.env.VITE_API_URL}`,
-        changeOrigin: true,  // 요청의 출처 헤더를 백엔드 주소로 변경
-        secure: false,
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      outDir: 'dist',  // Vite의 빌드 결과물이 'dist' 폴더에 생성됩니다.
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:8080',  // 환경 변수에서 API URL을 읽음, 기본값 설정
+          changeOrigin: true,  // 요청의 출처 헤더를 백엔드 주소로 변경
+          secure: false,  // HTTPS 사용 시 self-signed SSL을 허용
+        }
       }
     }
-  }
-})
+  };
+});
+
+// import { fileURLToPath, URL } from 'node:url'
+//
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+//
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   build: {
+//     // outDir: '../parkingplace-spring/src/main/resources/static'
+//     outDir: 'dist', // Vite의 빌드 결과물이 'dist' 폴더에 생성됩니다.
+//   },
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: `${import.meta.env.VITE_API_URL || 'http://localhost:8080'}`,  // 기본값 설정
+//         changeOrigin: true,  // 요청의 출처 헤더를 백엔드 주소로 변경
+//         secure: false,
+//       }
+//     }
+//   }
+// })
