15781b717e76bc238090499c7f1fad93de215b79
gaengmin <momo5736@naver.com>
2024-10-03 17:56:42 +0900

    Update UserMapCmp.vue

--- src/components/contents/user/map/UserMapCmp.vue
+++ src/components/contents/user/map/UserMapCmp.vue
@@ -188,7 +188,7 @@ export default {
 
       axios({
         method: 'get',
-        url: 'https://parkingback.store/api',
+        url: '/api/parkinglots',
         params: {
           minLat: bound.qa,
           maxLat: bound.pa,
