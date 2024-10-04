b55e05b36a51433b910c448a67adf726513c473d
rjsah09 <rjsah09@naver.com>
2024-10-04 09:19:09 +0900

    Fix: 이미지 수정

--- src/components/contents/user/map/UserMapCmp.vue
+++ src/components/contents/user/map/UserMapCmp.vue
@@ -213,8 +213,8 @@ export default {
           this.lots.forEach((item, index) => {
             //마커 형태 구분
             const imageSrc = item.hasUser
-              ? '../../../../assets/img/basic-lot-marker.png'
-              : '../../../../assets/img/marker-lot-no-owner.png'
+              ? '/public/basic-lot-marker.png'
+              : '/public/marker-lot-no-owner.png'
             const imageSize = new kakao.maps.Size(40, 40)
             const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize)
 
